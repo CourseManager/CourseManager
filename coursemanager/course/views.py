7cd739d81f3a13bae988e465880f10e336e8f315
cyice <1207201395@qq.com>
2018-10-15 08:33:16 +0800

    登陆页面。

--- coursemanager/course/views.py
+++ coursemanager/course/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 
 
 def index(request):
-    return render(request,'course/index.html')
+    return render(request, 'course/index.html')
+
+
+def select(request,page):
+    username = request.POST.get("username")
+    password = request.POST.get("password")
+    ret = {"username": username, "password": password}
+    return render(request, 'course/select.html', ret)
